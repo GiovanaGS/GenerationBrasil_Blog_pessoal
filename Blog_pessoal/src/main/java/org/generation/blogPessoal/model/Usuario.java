90fbbe6065fc8bb1fd45189316464b97caa8d3cd
unknown <geoo.vanna2305@gmail.com>
2022-03-09 18:05:47 -0300

    Implementando Testes com Junit

--- Blog_pessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ Blog_pessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -27,6 +27,22 @@ public class Usuario {
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String senha;
+	
+	private String foto;
+
+
+	public Usuario(long id, String nome, String foto, String usuario, String senha) {
+			this.id = id;
+			this.nome = nome;
+			this.foto = foto;
+			this.usuario = usuario;
+			this.senha = senha;
+			}
+
+	public Usuario() {
+		super();
+	}
+
 
 	public long getId() {
 		return id;
@@ -60,6 +76,13 @@ public class Usuario {
 		this.senha = senha;
 	}
 	
-	
+	public String getFoto() {
+		return foto;
+	}
+
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
 
 }
